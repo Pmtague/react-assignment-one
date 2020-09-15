6d876f705ac38431011f58dbe888c6d30834b062
Penny Tague <pmtague9@gmail.com>
2020-09-15 09:57:46 -0700

    Added UserInput and UserOutput components, state hooks, and two way binding for the input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+const App = props => {
+  const [username, setUsername] = useState({
+    users: [
+      {username: 'Jim'},
+      {username: 'Jane'},
+      {username: 'Joseph'},
+    ]
+  })
+
+  const changeHandler = (event) => {
+    setUsername({
+      users: [
+        {username: event.target.value},
+        {username: 'Jane'},
+        {username: 'Joseph'},
+      ]
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput changed={changeHandler} user={username.users[0].username}/>
+      <UserOutput user={username.users[0].username}/>
+      <UserOutput user={username.users[1].username}/>
+      <UserOutput user={username.users[2].username}/>
     </div>
   );
 }
